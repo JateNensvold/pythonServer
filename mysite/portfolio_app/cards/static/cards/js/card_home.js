4e06da8f47b274fe6a463947c274799e17dd3bf5
Nate Jensvold <jensvoldnate@gmail.com>
2021-01-29 13:26:05 -0800

    Working on template base, and switching development to laptop

--- mysite/portfolio_app/cards/static/cards/js/card_home.js
+++ mysite/portfolio_app/cards/static/cards/js/card_home.js
@@ -1,20 +1,63 @@
-window.onload = function() {
-  
-    var placeholder = document.querySelector('.placeholder'),
-        small = placeholder.querySelector('.img-small')
-    
-    // 1: load small image and show it
-    var img = new Image();
-    img.src = small.src;
-    img.onload = function () {
-     small.classList.add('loaded');
-    };
-    
-    // 2: load large image
-    var imgLarge = new Image();
-    imgLarge.src = placeholder.dataset.large; 
-    imgLarge.onload = function () {
-      imgLarge.classList.add('loaded');
-    };
-    placeholder.appendChild(imgLarge);
+// var isInViewport = function (elem) {
+//   var bounding = elem.getBoundingClientRect();
+//   return (
+//     bounding.top >= 0 &&
+//     bounding.left >= 0 &&
+//     bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//     bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+//   );
+// };
+// // var imgReplace = document.querySelector("h1");
+
+function LoadImage(elem) {
+  var img = new Image(),
+    x = elem;
+
+  img.onload = function () {
+    x.src = img.src;
+  };
+
+  img.src = elem.getAttribute("data-src")
+}
+
+function LoadBackground(elem) {
+  var img = new Image(),
+    x = elem;
+
+  img.onload = function () {
+    var temp = img.src;
+    console.log(temp);
+    var urlstring = 'url(' + temp + ')';
+    x.style.backgroundImage = urlstring;
+  };
+  // console.log(urlstring);
+  img.src = elem.getAttribute("data-src");
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+
+  var lazyElements = document.getElementsByClassName("lazy");
+  console.log(lazyElements);
+
+  console.log(lazyElements.length);
+  for (var i = 0; i < lazyElements.length; i++) {
+    if (lazyElements[i].tagName == 'IMG') {
+      LoadImage(lazyElements[i])
+    }
+    else if (lazyElements[i].tagName == 'BODY') {
+      LoadBackground(lazyElements[i])
+      console.log(lazyElements[i])
+    }
+    // var oldImg = lazyElements[i].getAttribute("src")
+    // var img = new Image(),
+    //   x = lazyElements[i];
+
+    // if (lazyElements[i].tagName == 'IMG') {
+    //   img.onload = function () {
+    //     console.log(x)
+    //     x.src = img.src;
+    //   };
+    //   img.src = lazyElements[i].getAttribute("data-src");
+    // }
   }
+});
--- mysite/portfolio_app/cards/views.py
+++ mysite/portfolio_app/cards/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from cards.models import CardPost
 # Create your views here.
+from django.views.decorators.cache import cache_page, cache_control
+
+# Set Cache to public, cache this view for 1 month
 
 
+@cache_page(86400)
+@cache_control(private=False, max_age=2629746)
 def card_home(request):
-    context = {"sports": {
-        "football": {
-            "image": "cards/img/base/football.jpg"},
-        "basketball": {
-            "image": "cards/img/base/basketball.jpg"},
-        "baseball": {
-            "image": "cards/img/base/baseball.jpg"}}}
+    context = {"path": "cards/img/base/",
+               "sports": {
+                   "football": {
+                       "name": "football"},
+                   "basketball": {
+                       "name": "basketball"},
+                   "baseball": {
+                       "name": "baseball"}}}
     return render(request, "card_home.html", context)
 
 
